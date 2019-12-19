ffef65cbbaf528b61ec9d52010a8ac58453fbcfe
Talha619 <hafiz.talhakh@gmail.com>
2019-12-19 14:41:59 +0500

    third commit

--- Github Test 2/Form1.Designer.cs
+++ Github Test 2/Form1.Designer.cs
@@ -66,7 +66,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "OK";
+            this.button2.Text = "Save";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox2
